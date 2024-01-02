63071a37b7d676c83dc72fcb2f71c50888d29b2e
Avi Kumar <149712920+frogsjumps@users.noreply.github.com>
2024-01-02 15:17:52 -0800

    Add files via upload

---
+++ AK.cs
@@ -0,0 +1,17 @@
+﻿string sentence;
+
+Console.WriteLine("Input a sentence: ");
+sentence = Console.ReadLine();
+
+int words = sentenceWords(sentence);
+
+Console.WriteLine("Your sentence has " + words + " words");
+
+static int sentenceWords(string sentence)
+{
+    string[] words = sentence.Split(' ');
+    int wordCount = words.Length;
+    return wordCount;
+}
+
+Console.ReadKey();
